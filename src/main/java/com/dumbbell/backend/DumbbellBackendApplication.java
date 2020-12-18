75166c2347471845bdb3477d0452ce1da10d65bb
dbenitez-bcn <dbr.bcn@gmail.com>
2020-12-18 12:49:58 +0100

    Initial commit

---
+++ src/main/java/com/dumbbell/backend/DumbbellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dumbbell.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbbellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbbellBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dumbbell/backend/DumbbellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumbbell.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumbbellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
