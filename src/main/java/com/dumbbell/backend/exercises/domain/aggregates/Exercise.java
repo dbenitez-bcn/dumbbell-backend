664250fde69c09ad4fa342260918a27aefa24629
dbenitez-bcn <dbr.bcn@gmail.com>
2021-01-03 21:52:39 +0100

    renames Exercise aggregate

--- src/main/java/com/dumbbell/backend/exercises/domain/aggregates/Exercise.java
+++ src/main/java/com/dumbbell/backend/exercises/domain/aggregates/Exercise.java
@@ -5,13 +5,13 @@ import com.dumbbell.backend.exercises.domain.valueObjects.Difficulty;
 import com.dumbbell.backend.exercises.domain.valueObjects.ExerciseId;
 import com.dumbbell.backend.exercises.domain.valueObjects.Name;
 
-public class Exercises {
+public class Exercise {
     private final ExerciseId id;
     private final Name name;
     private final Description description;
     private final Difficulty difficulty;
 
-    public Exercises(String name, String description, int difficulty) {
+    public Exercise(String name, String description, int difficulty) {
         this.id = null;
         this.name = new Name(name);
         this.description = new Description(description);
