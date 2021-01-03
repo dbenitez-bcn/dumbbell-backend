d014b2680d7db0543d529d0cc376ff2254e17207
dbenitez-bcn <dbr.bcn@gmail.com>
2021-01-03 01:07:32 +0100

    adds exercises exceptions

---
+++ src/main/java/com/dumbbell/backend/exercises/domain/exceptions/InvalidDescription.java
@@ -0,0 +1,4 @@
+package com.dumbbell.backend.exercises.domain.exceptions;
+
+public class InvalidDescription extends RuntimeException {
+}
---
+++ src/main/java/com/dumbbell/backend/exercises/domain/exceptions/InvalidDifficulty.java
@@ -0,0 +1,4 @@
+package com.dumbbell.backend.exercises.domain.exceptions;
+
+public class InvalidDifficulty extends RuntimeException {
+}
