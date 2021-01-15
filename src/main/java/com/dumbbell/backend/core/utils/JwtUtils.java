9572dd0b91bf3ea4d038a26dcd20c5e4b7818f1d
dbenitez-bcn <dbr.bcn@gmail.com>
2021-01-15 01:12:15 +0100

    cleans JwtUtils code

--- src/main/java/com/dumbbell/backend/core/utils/JwtUtils.java
+++ src/main/java/com/dumbbell/backend/core/utils/JwtUtils.java
@@ -16,6 +16,20 @@ public class JwtUtils {
     @Value("${SECRET_KEY:DEFAULT_KEY}")
     private String SECRET_KEY;
 
+    public String generateToken(String subject, Map<String, Object> claims) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            return (isTokenSigned(token) && !isTokenExpired(token));
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
     public String extractSubject(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -25,11 +39,11 @@ public class JwtUtils {
         return claims.get("role", String.class);
     }
 
-    public Date extractExpiration(String token) {
+    private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    private  <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
@@ -40,16 +54,6 @@ public class JwtUtils {
 
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
-    }
-
-    public String generateToken(String subject, Map<String, Object> claims) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-    }
-
-    public Boolean validateToken(String token) {
-        return (isTokenSigned(token) && !isTokenExpired(token));
     }
 
     private boolean isTokenSigned(String token) {
