d3ddb6fcb002031c85aec0e98db2bc9d6b60d692
dbenitez-bcn <dbr.bcn@gmail.com>
2021-01-17 19:30:37 +0100

    adds cors configurer

---
+++ src/main/java/com/dumbbell/backend/core/presentation/config/CorsConfigurer.java
@@ -0,0 +1,20 @@
+package com.dumbbell.backend.core.presentation.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Component
+public class CorsConfigurer implements CorsConfigurationSource {
+    @Override
+    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+        return configuration;
+    }
+}
--- src/main/java/com/dumbbell/backend/core/presentation/config/SecurityConfigurer.java
+++ src/main/java/com/dumbbell/backend/core/presentation/config/SecurityConfigurer.java
@@ -3,25 +3,25 @@ package com.dumbbell.backend.core.presentation.config;
 import com.dumbbell.backend.core.presentation.filters.AuthenticationFilter;
 import com.dumbbell.backend.core.presentation.filters.TokenValidationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     private final TokenValidationFilter tokenValidationFilter;
     private final AuthenticationFilter authenticationFilter;
+    private final CorsConfigurer corsConfigurer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().configurationSource(corsConfigurer)
+                .and()
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(tokenValidationFilter, AuthenticationFilter.class)
                 .authorizeRequests()
@@ -30,19 +30,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/exercise").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/exercise/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/exercise/**").hasAuthority("ADMIN")
-                .anyRequest().authenticated()
+                .antMatchers("/").authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000");
-            }
-        };
     }
 }
