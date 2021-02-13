e0dcbf27c46e904a589e05b649d37c3a4e1fc1c8
dbenitez-bcn <dbr.bcn@gmail.com>
2021-02-14 00:43:27 +0100

    adds toggle service

---
+++ src/main/java/com/dumbbell/backend/toggles/application/ToggleService.java
@@ -0,0 +1,9 @@
+package com.dumbbell.backend.toggles.application;
+
+import com.dumbbell.backend.toggles.domain.aggregates.FeatureToggle;
+
+public class ToggleService {
+    public FeatureToggle create(String name, boolean value) {
+        return null;
+    }
+}
