40ad1f115a38d653d13508c8c2aff9c90b885e84
dbenitez-bcn <dbr.bcn@gmail.com>
2021-02-14 00:42:01 +0100

    adds toggle response

---
+++ src/main/java/com/dumbbell/backend/toggles/presentation/responses/ToggleResponse.java
@@ -0,0 +1,9 @@
+package com.dumbbell.backend.toggles.presentation.responses;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ToggleResponse {
+    public final String name;
+    public final boolean value;
+}
