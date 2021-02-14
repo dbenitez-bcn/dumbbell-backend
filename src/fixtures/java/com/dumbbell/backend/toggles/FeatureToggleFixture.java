87929f640c2fde9f7ee9614a11752e78f9b8fc03
dbenitez-bcn <dbr.bcn@gmail.com>
2021-02-14 15:55:41 +0100

    adds toggle entity to fixtures

--- src/fixtures/java/com/dumbbell/backend/toggles/FeatureToggleFixture.java
+++ src/fixtures/java/com/dumbbell/backend/toggles/FeatureToggleFixture.java
@@ -1,6 +1,7 @@
 package com.dumbbell.backend.toggles;
 
 import com.dumbbell.backend.toggles.domain.aggregates.FeatureToggle;
+import com.dumbbell.backend.toggles.infrastructure.postgresql.entities.FeatureToggleEntity;
 
 public class FeatureToggleFixture {
     public static final String TOGGLE_NAME = "DEFAULT_TOGGLE_NAME";
@@ -8,5 +9,9 @@ public class FeatureToggleFixture {
 
     public static FeatureToggle testToggle() {
         return new FeatureToggle(TOGGLE_NAME, TOGGLE_VALUE);
+    }
+
+    public static FeatureToggleEntity testToggleEntity() {
+        return new FeatureToggleEntity(TOGGLE_NAME, TOGGLE_VALUE);
     }
 }
