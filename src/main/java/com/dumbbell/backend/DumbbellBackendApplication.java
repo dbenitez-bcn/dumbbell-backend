e4c622ace129b28938b93029abb6ca54758176e1
David Benitez <dbr.bcn@gmail.com>
2022-04-09 16:06:44 +0200

    adds Flyway configuration bean

--- src/main/java/com/dumbbell/backend/DumbbellBackendApplication.java
+++ src/main/java/com/dumbbell/backend/DumbbellBackendApplication.java
@@ -1,5 +1,7 @@
 package com.dumbbell.backend;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,11 @@ public class DumbbellBackendApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public FluentConfiguration flywayConfig() {
+		return Flyway.configure();
 	}
 
 	public static void main(String[] args) {
